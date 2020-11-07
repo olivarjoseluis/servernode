a52dd4e43857fa3d217d34dce6c2914b388409ee
Jose Luis Olivar <jose.olivar@javerianacali.edu.co>
2020-11-06 19:09:16 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+//Solo NODE
+/*const { read } = require('fs');
+const http = require('http');
+const hostmname = '127.0.0.1';
+const port = 8000;
+
+const serve = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hola Mundo');
+});
+
+serve.listen(port, hostmname, () => {
+    console.log(`Servidor corriendo en http://${hostmname}:${port}`);
+});
+*/
